bf4f782454dfbc3c306554b8d5e4eccda04bd5a9
Artem <31787423+DokSna@users.noreply.github.com>
2022-04-23 17:41:59 +0300

    Вёрстка блока TextField

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,11 +6,46 @@ class WeatherForecast extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.red,
       appBar: AppBar(
+        backgroundColor: Colors.red,
+        centerTitle: true,
+        elevation: 0.0, //убираем тень
         title: const Text(
           'Weather Forecast',
+          style: TextStyle(color: Colors.white),
         ),
       ),
+      body: _buildBody(),
     );
   }
+}
+
+Widget _buildBody() {
+  return SingleChildScrollView(
+      child: Column(
+    children: <Widget>[
+      _searchBox(),
+    ],
+  ));
+}
+
+// TextField
+_searchBox() {
+  return Row(children: const [
+    SizedBox(
+      width: 300,
+      child: TextField(
+        // obscureText: true,
+        decoration: InputDecoration(
+          icon: Icon(Icons.search),
+          iconColor: Colors.white,
+          border: InputBorder.none,
+          // border: OutlineInputBorder(),
+          labelText: 'Enter City Name',
+          labelStyle: TextStyle(color: Colors.white),
+        ),
+      ),
+    ),
+  ]);
 }
